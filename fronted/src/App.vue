34535bf6949d90af96417342cd5044a4231cb004
kun368 <kun368@126.com>
2018-03-24 19:29:37 +0800

    完成前端

--- fronted/src/App.vue
+++ fronted/src/App.vue
@@ -1,23 +1,33 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container direction="horizontal">
+      <el-main>
+
+        <h1><i class="el-icon-circle-check"> </i> 短网址服务</h1>
+        <h5>简单 / 易用 / 便捷</h5>
+        <h5>Powered By zzkun.com</h5>
+        <main-converter/>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import MainConverter from "./components/MainConverter";
+
+  export default {
+    components: {MainConverter},
+    name: 'App',
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
